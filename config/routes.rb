57736784dbc2395f441165a509a6cf5a76cf0607
KumikoKawase <kumikokawase@kawasetakayukis-MacBook-Air.local>
2021-05-14 22:48:24 +0900

    make login signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
 
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get '/signup', to: 'users#new'
   root 'static_pages#home'
   
   get 'static_pages/home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
+  
 end
